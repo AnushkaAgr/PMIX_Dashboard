587e3444e13135d590676f35f6c3a0b320db1244
Anurag-2312 <sanus0216@gmail.com>
2026-05-25 20:50:07 +0530

    Fix Vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/data/**': ['./data/**'],
-      '/api/sheets': ['./data/**'],
-    },
+  outputFileTracingIncludes: {
+    '/api/data/**': ['./data/**'],
+    '/api/sheets': ['./data/**'],
   },
 };
 
